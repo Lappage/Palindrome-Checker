e20120e51ffd95c2b8545c25c41909598d3a541a
Lee Lappage <114675964+Lappage@users.noreply.github.com>
2024-01-16 12:49:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const searchBox = document.getElementById("text-input");
+const CheckButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+const checkValue = () => {
+  const value = searchBox.value;
+  const cleanValue = value.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const valueReversed = cleanValue.split("").reverse().join("");
+
+  if (value.trim() === "") {
+    alert("Please input a value");
+    return;
+  }
+
+  if (cleanValue === valueReversed) {
+    result.innerHTML = `<p><b>${value}</b> is a palindrome.</p>`;
+  } else {
+    result.innerHTML = `<p><b>${value}</b> is not a palindrome.</p>`;
+  }
+};
+
+searchBox.addEventListener("keypress", (buttonPress) => {
+  if (buttonPress.keyCode === 13) {
+    checkValue();
+  }
+});
